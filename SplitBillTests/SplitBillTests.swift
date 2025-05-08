6d79fae60697a04d8abcfd20854463c2be8f1a4b
Reza Juliandri <rezajuliandri20@gmail.com>
2025-05-09 00:14:17 +0800

    Initial Commit

---
+++ SplitBillTests/SplitBillTests.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBillTests.swift
+//  SplitBillTests
+//
+//  Created by Reza Juliandri on 09/05/25.
+//
+
+import Testing
+@testable import SplitBill
+
+struct SplitBillTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

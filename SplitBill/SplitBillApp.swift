80078ee9fbe297607bb68805a642234bbae83b14
Reza Juliandri <rezajuliandri20@gmail.com>
2025-05-18 00:44:34 +0800

    chore: update with firebase

--- SplitBill/SplitBillApp.swift
+++ SplitBill/SplitBillApp.swift
@@ -7,9 +7,22 @@
 
 import SwiftUI
 import SwiftData
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct SplitBillApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var sharedModelContainer: ModelContainer = {
         let isPreview = ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
         let schema = Schema([
